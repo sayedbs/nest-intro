771f47b6ac0fd3593e3eb6261b3de5bffa96a332
sayedbs <sayeed@brainstation-23.com>
2026-02-18 13:06:44 +0600

    Default value and integer pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,21 +1,31 @@
-import { Controller, Get, Post, Put, Patch, Delete, Param, Query, Req } from '@nestjs/common';
+import { Controller, Get, Post, Put, Patch, Delete, Param, Query, Req, Header, Headers, Ip, ParseIntPipe, DefaultValuePipe } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get()
-    findAll(): string {
+    findAll(
+        @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: Number, 
+        @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: Number
+    ) {
+        console.log(typeof limit, limit);
+        console.log(typeof page, page);
+
         return 'This action returns all users';
     }
 
     @Get('/:id')
-    findOne(@Param() params: any, @Query() query: any): string {
-        console.log(params, query);
-        return `This action returns a ${params.id} user`;
+    findOne(@Param('id') id: any, @Query() query: any): string {
+        console.log(id, query);
+        return `This action returns a ${id} user`;
     }
 
     @Post()
-    create(@Req() request: any): string {
-        console.log(request.body);
+    createUser(
+        @Req() request: any,
+        @Headers() headers: any,
+        @Ip() ip: string
+    ): string {
+        console.log(request.body, headers, ip);
         return 'This action adds a new user';
     }
 }
